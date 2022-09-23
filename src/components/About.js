829aa414e948c5038b7813ccc841cb2ce6e38022
KipyegonB <koechbernard08@gmail.com>
2022-09-23 14:08:00 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import { image } from "../data/data";
 function About() {
   return <div id="about">
     <h1>About Me</h1>
-    <p></p>
+    <p>I made this </p>
     <img src={image} alt="I made this"/>
   </div>
 }
